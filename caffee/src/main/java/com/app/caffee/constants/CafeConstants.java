055a7e4665422d103021511525bcaec13fa3f518
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2025-06-24 00:01:39 +0700

    :memo: Update API for Bill

--- caffee/src/main/java/com/app/caffee/constants/CafeConstants.java
+++ caffee/src/main/java/com/app/caffee/constants/CafeConstants.java
@@ -4,5 +4,5 @@ public class CafeConstants {
     public static final String SOMETHING_WENT_WRONG = "Something went wrong.";
     public static final String INVALID_DATA = "Invalid Data.";
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
-
+    public static final String STORE_LOCATION = "E:\\IT_Field_Learning\\WebDevelopment\\ProjectFE\\Angular\\CafeManagementSystem\\cafe-store-files";
 }
